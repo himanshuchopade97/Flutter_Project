1f4e914295aa88345eb35844684dc81c21d911ab
himanshuchopade <himanshuchopade97@gmail.com>
2024-06-19 18:47:25 +0530

    Animdo commit

---
+++ animdo/lib/pages/home_page.dart
@@ -0,0 +1,114 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+// ignore: use_key_in_widget_constructors
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomePageState();
+  }
+}
+
+class _HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  double _buttonRadius = 100;
+
+  final Tween<double> _backgroundScale = Tween<double>(begin: 0.0, end: 1.0);
+
+  AnimationController? _starIconAnimationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _starIconAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 4),
+    );
+    _starIconAnimationController!.repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          _pageBackground(),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _circularAnimationButoon(),
+              _starIcon(),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _pageBackground() {
+    return TweenAnimationBuilder(
+      tween: _backgroundScale,
+      duration: const Duration(seconds: 1),
+      // ignore: no_leading_underscores_for_local_identifiers
+      builder: (_context, _scale, _child) {
+        return Transform.scale(
+          scale: _scale,
+          child: _child,
+        );
+      },
+      curve: Curves.easeInOutCubicEmphasized,
+      child: Container(
+        color: Colors.blue,
+      ),
+    );
+  }
+
+  Widget _circularAnimationButoon() {
+    return Center(
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            _buttonRadius += _buttonRadius == 200 ? -100 : 100;
+          });
+        },
+        child: AnimatedContainer(
+          duration: const Duration(seconds: 2),
+          curve: Curves.bounceInOut,
+          height: _buttonRadius,
+          width: _buttonRadius,
+          decoration: BoxDecoration(
+            color: Colors.purple,
+            borderRadius: BorderRadius.circular(_buttonRadius),
+          ),
+          child: const Center(
+            child: Text(
+              "Basic",
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _starIcon() {
+    return AnimatedBuilder(
+      animation: _starIconAnimationController!.view,
+      // ignore: no_leading_underscores_for_local_identifiers
+      builder: (_buildContext, _child) {
+        return Transform.rotate(
+          angle: _starIconAnimationController!.value * 2 * pi,
+          child: _child,
+        );
+      },
+      child: const Icon(
+        Icons.star,
+        size: 100,
+        color: Colors.white,
+      ),
+    );
+  }
+}
